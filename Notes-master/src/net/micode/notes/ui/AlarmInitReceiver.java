2ee2757fd2b617502f86498f7eb550929fe1f02c
Sing How <202000130138@mail.sdu.edu.cn>
2023-03-03 20:30:49 +0800

    宋璎航阅读

--- Notes-master/src/net/micode/notes/ui/AlarmInitReceiver.java
+++ Notes-master/src/net/micode/notes/ui/AlarmInitReceiver.java
@@ -1,19 +1,4 @@
-/*
- * Copyright (c) 2010-2011, The MiCode Open Source Community (www.micode.net)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+//read by 宋璎航
 package net.micode.notes.ui;
 
 import android.app.AlarmManager;
@@ -31,21 +16,25 @@ import net.micode.notes.data.Notes.NoteColumns;
 public class AlarmInitReceiver extends BroadcastReceiver {
 
     private static final String [] PROJECTION = new String [] {
-        NoteColumns.ID,
-        NoteColumns.ALERTED_DATE
+            NoteColumns.ID,
+            NoteColumns.ALERTED_DATE
     };
-
+    //对数据库的操作，调用标签ID和闹钟时间
     private static final int COLUMN_ID                = 0;
     private static final int COLUMN_ALERTED_DATE      = 1;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         long currentDate = System.currentTimeMillis();
+        //System.currentTimeMillis()产生一个当前的毫秒
+        //这个毫秒其实就是自1970年1月1日0时起的毫秒数
         Cursor c = context.getContentResolver().query(Notes.CONTENT_NOTE_URI,
                 PROJECTION,
                 NoteColumns.ALERTED_DATE + ">? AND " + NoteColumns.TYPE + "=" + Notes.TYPE_NOTE,
                 new String[] { String.valueOf(currentDate) },
+                //将long变量currentDate转化为字符串
                 null);
+        //Cursor在这里的作用是通过查找数据库中的标签内容，找到和当前系统时间相等的标签
 
         if (c != null) {
             if (c.moveToFirst()) {
@@ -61,5 +50,8 @@ public class AlarmInitReceiver extends BroadcastReceiver {
             }
             c.close();
         }
+        //然而通过网上查找资料发现，对于闹钟机制的启动，通常需要上面的几个步骤
+        //如新建Intent、PendingIntent以及AlarmManager等
+        //这里就是根据数据库里的闹钟时间创建一个闹钟机制
     }
 }
