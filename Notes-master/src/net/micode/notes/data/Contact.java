649fc6055dba3bfcb64d832f5ff6f143227f5810
Zzzclown <72583906+Zzzclown@users.noreply.github.com>
2023-03-06 18:24:29 +0800

    Update Contact.java

--- Notes-master/src/net/micode/notes/data/Contact.java
+++ Notes-master/src/net/micode/notes/data/Contact.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (c) 2010-2011, The MiCode Open Source Community (www.micode.net)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//read by 曾梦媛
 
 package net.micode.notes.data;
 
@@ -25,28 +11,35 @@ import android.util.Log;
 
 import java.util.HashMap;
 
+//联系人
 public class Contact {
     private static HashMap<String, String> sContactCache;
     private static final String TAG = "Contact";
 
+    //定义字符串CALLER_ID_SELECTION 的内容
     private static final String CALLER_ID_SELECTION = "PHONE_NUMBERS_EQUAL(" + Phone.NUMBER
     + ",?) AND " + Data.MIMETYPE + "='" + Phone.CONTENT_ITEM_TYPE + "'"
     + " AND " + Data.RAW_CONTACT_ID + " IN "
             + "(SELECT raw_contact_id "
             + " FROM phone_lookup"
             + " WHERE min_match = '+')";
-
+    
+    // 查找联系人
     public static String getContact(Context context, String phoneNumber) {
         if(sContactCache == null) {
             sContactCache = new HashMap<String, String>();
         }
-
+        
+        // 在HashMap中查找phoneNumber信息，若有则返回
         if(sContactCache.containsKey(phoneNumber)) {
             return sContactCache.get(phoneNumber);
         }
-
+        
+        //HashMap中没有phoneNumber信息，修改字符串CALLER_ID_SELECTION中Data.RAW_CONTACT_ID对应内容
         String selection = CALLER_ID_SELECTION.replace("+",
                 PhoneNumberUtils.toCallerIDMinMatch(phoneNumber));
+        
+        //在数据库中查找phoneNumber的信息
         Cursor cursor = context.getContentResolver().query(
                 Data.CONTENT_URI,
                 new String [] { Phone.DISPLAY_NAME },
@@ -56,16 +49,19 @@ public class Contact {
 
         if (cursor != null && cursor.moveToFirst()) {
             try {
+                //找到phoneNumber的信息
                 String name = cursor.getString(0);
                 sContactCache.put(phoneNumber, name);
                 return name;
             } catch (IndexOutOfBoundsException e) {
+                //出现异常
                 Log.e(TAG, " Cursor get string error " + e.toString());
                 return null;
             } finally {
                 cursor.close();
             }
         } else {
+            //没有找到phoneNumber的信息
             Log.d(TAG, "No contact matched with number:" + phoneNumber);
             return null;
         }
